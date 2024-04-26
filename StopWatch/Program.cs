6f8c7496f948df89254e21ce64ffe9529109f7a1
gritNgo <dustspeck00@gmail.com>
2024-04-26 17:33:55 +0200

    Add project files.

---
+++ StopWatch/Program.cs
@@ -0,0 +1,49 @@
+﻿Stopwatch stopwatch = new Stopwatch();
+
+while (true)
+{
+    if (stopwatch.IsRunning == false)
+    {
+        Console.Write("Enter 'start' to start stopwatch or 'exit' to exit program: ");
+    }
+
+    else
+    {
+        Console.Write("Enter 'stop' to stop stopwatch or 'exit' to exit program: ");
+    }
+    var input = Console.ReadLine();
+
+    if (input.ToLower() == "start" && stopwatch.IsRunning == false)
+    {
+        stopwatch.IsRunning = true;
+        var startTime = stopwatch.Start();
+        Console.WriteLine("Started. " + startTime.ToString("T"));
+    }
+
+    else if (input.ToLower() == "start" && stopwatch.IsRunning == true)
+    {
+        throw new InvalidOperationException("Stopwatch already running.");
+    }
+    else if (input.ToLower() == "stop" && stopwatch.IsRunning == false)
+    {
+        throw new InvalidOperationException("Stopwatch already stopped.");
+    }
+
+    else if (input.ToLower() == "stop")
+    {
+        stopwatch.IsRunning = false;
+        var endTime = stopwatch.Stop();
+        Console.WriteLine("Stopped. Duration: " + stopwatch.Duration);
+    }
+
+    else if (input.ToLower() == "exit")
+    {
+        break;
+    }
+
+    else
+    {
+        Console.WriteLine("Invalid Input.");
+        continue;
+    }
+}
---
+++ StopWatch/Stopwatch.cs
@@ -0,0 +1,32 @@
+﻿using System.Numerics;
+
+public class Stopwatch
+{
+    public DateTime StartTime { get; set; }
+    public DateTime StopTime { get; set; }
+    public bool IsRunning = false;
+
+
+    public TimeSpan Duration
+    {
+        get
+        {
+            var duration = StopTime - StartTime;
+            return duration;
+        }
+    }
+
+    public DateTime Start()
+    {
+        var startTime = DateTime.Now;
+        StartTime = startTime;
+        return startTime;
+    }
+
+    public DateTime Stop()
+    {
+        var stopTime = DateTime.Now;
+        StopTime = stopTime;
+        return stopTime;
+    }
+}
